c4b6774d505c94cfbc49310f1c656d2bdee0acf5
Arthur Koba <caprimores@gmail.com>
2023-02-10 16:05:08 +0300

    bug fix | correct service name change branch to master

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Updating Project with GitHub. Project directory - `pwd`"
+service_filename="tsutmb-hostel.service"
+
+echo "Updating Project (`service_filename`) with GitHub. Project directory - `pwd`"
 source venv/bin/activate
-git pull origin main
-systemctl restart gunicorn.service
+git pull origin master
+systemctl restart service_filename
