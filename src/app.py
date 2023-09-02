a7887bbc88b3cde569dfb8260637d4e852fb3656
Arthur Koba <caprimores@gmail.com>
2023-09-02 18:09:49 +0300

    refactor, fix exception destroying tasks

--- src/app.py
+++ src/app.py
@@ -1,14 +1,27 @@
-from asyncio import new_event_loop, AbstractEventLoop
+from asyncio import new_event_loop, AbstractEventLoop, Task
 
 from core.config import get_config
 from core.loggers import main_logger
 from core.vk.conversation import GroupConversation
 
 
-async def main(loop: AbstractEventLoop) -> None:
+def exception_handler(_loop: AbstractEventLoop, context):
+    exception = context.get("exception")
+    if context.get('message') == "Task was destroyed but it is pending!" and type(context.get('task')) == Task:
+        task: Task = context.get('task')
+        main_logger.warning(f"Task destroyed: {task.get_name()} ({task.get_coro()})")
+        return
+    main_logger.critical(f"Exception: {context.get('message', 'NO MSG')}")
+    raise exception
+
+
+if __name__ == '__main__':
     main_logger.info("Запуск приложения.")
+    loop = new_event_loop()
+    loop.set_exception_handler(exception_handler)
 
     configs = get_config()
+
     group_access_token = configs.get("Tokens", "group_access_token")
     conversation_id = configs.getint("Conversation", "conversation_id")
     notification_join_offset = configs.getint("Conversation", "notification_join_offset")
@@ -19,24 +32,10 @@ async def main(loop: AbstractEventLoop) -> None:
         notification_join_offset=notification_join_offset
     )
 
-    loop.create_task(group_conversation.start())
-
-
-def exception_handler(loop: AbstractEventLoop, context):
-    exception = context.get("exception")
-    loop.stop()
-    raise exception
-
-
-def start() -> None:
-    loop = new_event_loop()
-    loop.set_exception_handler(exception_handler)
-    loop.create_task(main(loop))
+    group_conversation_task: Task = loop.create_task(group_conversation.start())
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         main_logger.warning("Завершение работы программы!")
-
-
-if __name__ == '__main__':
-    start()
+    except BaseException as e:
+        main_logger.critical(f"Программа остановлена по ошибке: {e}")
