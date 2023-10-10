9fe954f58f215d85631d5e6a7206dc0e8b8d2665
Arthur Koba <caprimores@gmail.com>
2023-10-10 20:54:37 +0300

    add error and debug log handlers, new logs path

--- src/core/loggers.py
+++ src/core/loggers.py
@@ -1,5 +1,9 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from colorlog import ColoredFormatter
+from os import path
+
+from core.config import DEFAULT_LOGS_DIRECTORY_PATH
 
 string_format = "%(log_color)s%(levelname)-8.8s|%(asctime)s| %(name)-18s| - %(message)s%(reset)s"
 date_format = "%Y-%m-%d %H:%M:%S"
@@ -9,11 +13,22 @@ formatter = ColoredFormatter(fmt=string_format, datefmt=date_format)
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 
+file_handler_formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | - %(message)s")
+path_log_debug = path.join(DEFAULT_LOGS_DIRECTORY_PATH, "tsutmb-hostel_debug.log")
+path_log_error = path.join(DEFAULT_LOGS_DIRECTORY_PATH, "tsutmb-hostel_errors.log")
+file_handler_debug = RotatingFileHandler(filename=path_log_debug, maxBytes=1024 * 10, backupCount=3)
+file_handler_errors = RotatingFileHandler(path_log_error, maxBytes=1024 * 10, backupCount=3)
+file_handler_debug.setFormatter(file_handler_formatter)
+file_handler_errors.setFormatter(file_handler_formatter)
+file_handler_debug.setLevel(logging.DEBUG)
+file_handler_errors.setLevel(logging.ERROR)
+
+
 logging.basicConfig(
     level=logging.DEBUG,
     format=string_format,
     datefmt=date_format,
-    handlers=[handler]
+    handlers=[handler, file_handler_debug, file_handler_errors]
 )
 
 logging.getLogger("asyncio").setLevel(logging.INFO)
