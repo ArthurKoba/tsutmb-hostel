1b346d1dff0428542b1e96ad6d39446f5ec02e12
ArthurKoba <caprimores@gmail.com>
2024-12-23 00:32:58 +0300

    adaptation script for new packages

--- src/core/vk/base/bot.py
+++ src/core/vk/base/bot.py
@@ -1,8 +1,7 @@
-import asyncio
 from typing import TYPE_CHECKING, Optional
 
-from vkbottle import Bot
-from vkbottle.framework.labeler import UserLabeler
+from aiohttp import ClientSession, ClientResponse
+from vkbottle import Bot, API, SingleAiohttpClient
 from vkbottle.modules import logger
 from vkbottle.polling.user_polling import UserPolling
 from vkbottle.tools import LoopWrapper
@@ -17,6 +16,24 @@ if TYPE_CHECKING:
     from vkbottle.dispatch import ABCRouter, ABCStateDispenser
     from vkbottle.framework.labeler import ABCLabeler
 
+
+class NoSSLAiohttp(SingleAiohttpClient):
+    async def request_raw(
+        self,
+        url: str,
+        method: str = "GET",
+        data: Optional[dict] = None,
+        **kwargs,
+    ) -> "ClientResponse":
+        if not self.session:
+            self.session = ClientSession(
+                json_serialize=self.json_processing_module.dumps,
+                **self._session_params,
+            )
+        kwargs.setdefault("ssl", False)
+        async with self.session.request(url=url, method=method, data=data, **kwargs) as response:
+            await response.read()
+            return response
 
 class BotMessagesPooling(UserPolling):
     """The bot uses the User Long Poll to get its events.
@@ -38,23 +55,28 @@ class BotMessagesPooling(UserPolling):
     async def get_server(self) -> dict:
         logger.debug("Getting polling server...")
         if self.group_id is None:
-            self.group_id = (await self.api.request("groups.getById", {}))["response"][0]["id"]
+            result = await self.api.request("groups.getById", {})
+            self.group_id = result["response"][0]["id"]
         return (await self.api.request("messages.getLongPollServer", {}))["response"]
 
 
 class BotUserLongPool(Bot):
     def __init__(self, token: Optional["Token"] = None, api: Optional["ABCAPI"] = None,
                  polling: Optional["ABCPolling"] = None, callback: Optional["ABCCallback"] = None,
-                 loop: Optional[asyncio.AbstractEventLoop] = None, loop_wrapper: Optional[LoopWrapper] = None,
-                 router: Optional["ABCRouter"] = None, labeler: Optional["ABCLabeler"] = None,
-                 state_dispenser: Optional["ABCStateDispenser"] = None, conversation_id: int = None,
-                 error_handler: Optional["ABCErrorHandler"] = None, task_each_event: bool = True):
+                 loop_wrapper: Optional[LoopWrapper] = None, router: Optional["ABCRouter"] = None,
+                 labeler: Optional["ABCLabeler"] = None, state_dispenser: Optional["ABCStateDispenser"] = None,
+                 conversation_id: int = None, error_handler: Optional["ABCErrorHandler"] = None):
 
         polling = polling or BotMessagesPooling()
         labeler = labeler or Labeler(conversation_id=conversation_id)
 
-        super().__init__(token, api, polling, callback, loop, loop_wrapper, router, labeler, state_dispenser,
-                         error_handler, task_each_event)
+        http_client = NoSSLAiohttp()
+        api = API(token=token, http_client=http_client)
+
+        super().__init__(
+            api=api, polling=polling, callback=callback, loop_wrapper=loop_wrapper,
+            router=router, labeler=labeler, state_dispenser=state_dispenser,
+            error_handler=error_handler)
 
     @property
     def on(self) -> Labeler:
